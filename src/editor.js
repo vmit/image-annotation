4973d072adda160d6b486c32471c9e5d22b4654e
Dmitry Vykochko <dmitry.vykochko@gmail.com>
2017-03-29 16:41:07 +0300

    Editor's base logic

---
+++ src/editor.js
@@ -0,0 +1,71 @@
+import editorMarkup from './editor.html';
+import Keys from './utils/keys';
+import {PolygonEditor, NewPolygonEditor} from './shape-editors/polygon-editor';
+import DEFAULT_SHAPE_STYLE  from './default-shapes-style.json';
+
+
+const SHAPE_EDITORS = {
+    polygon: PolygonEditor
+}
+
+
+export default class Editor {
+    constructor(shapes = []) {
+        this._el = {};
+        this._shapes = shapes;
+        this._activeShapeEditor = null;
+    }
+
+    onNewShape(shape) {
+        this._activeShapeEditor = null;
+        this._shapes.push(shape);
+        this.renderShape(shape);
+    }
+
+    renderInto(container) {
+        container.innerHTML = editorMarkup;
+
+        this._el.canvas = container.querySelector('.image-annotation-editor__canvas');
+        this._el.polygon = container.querySelector('.image-annotation-editor__shape-polygon');
+
+        this._el.polygon.addEventListener('click', () => {
+            this._activeShapeEditor = new NewPolygonEditor(this._el.canvas, DEFAULT_SHAPE_STYLE);
+            this._activeShapeEditor.render();
+            this._activeShapeEditor.on('shape:new', this.onNewShape.bind(this));
+        });
+
+        this._el.canvas.addEventListener('click', (e) => {
+            if (this._activeShapeEditor) {
+                this._activeShapeEditor.onCanvasClick(e.offsetX, e.offsetY);
+            }
+        });
+
+        this._el.canvas.addEventListener('mousemove', (e) => {
+            if (this._activeShapeEditor) {
+                this._activeShapeEditor.onCanvasMouseMove(e.offsetX, e.offsetY);
+            }
+        });
+
+        this._el.canvas.addEventListener('keydown', (e) => {
+            let key;
+            if (this._activeShapeEditor && (key = Keys.fromKeyCode(e.keyCode))) {
+                this._activeShapeEditor.onCanvasKeyPressed(key);
+            }
+        });
+
+        this._shapes.forEach((shape) => {
+            this.renderShape(shape);
+        });
+    }
+
+    renderShape(shape) {
+        const ShapeEditorClass = SHAPE_EDITORS[shape.type];
+
+        if (!ShapeEditorClass) {
+            console.warn(`Unknown shape type: ${shape.type}`);
+            return;
+        }
+
+        new ShapeEditorClass(this._el.canvas, shape, DEFAULT_SHAPE_STYLE).render();
+    }
+}
