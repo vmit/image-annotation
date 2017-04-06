87c948368b9785207c7de209112756b1c1c9033f
Dmitry Vykochko <dmitry.vykochko@gmail.com>
2017-04-06 12:11:57 +0300

    Comment ThrottledProvider

--- src/utils/throttled-provider.js
+++ src/utils/throttled-provider.js
@@ -1,16 +1,26 @@
+/**
+ * Simple cache of one value. Could be used for heavily computed values in frequent calls (like "mousemove" handler).
+ */
 export default class ThrottledProvider {
+    /**
+     * @param {function} providerFn
+     * @param {number} [wait=1000]
+     */
     constructor(providerFn, wait=1000) {
         this._providerFn = providerFn;
         this._wait = wait;
-        this._latestCall = 0;
+        this._latestCallTime = 0;
         this._latestValue = null;
     }
 
+    /**
+     * @return {*} value returned from providerFn
+     */
     get() {
         const now = Date.now();
 
-        if (now - this._latestCall > this._wait) {
-            this._latestCall = now;
+        if (now - this._latestCallTime > this._wait) {
+            this._latestCallTime = now;
             this._latestValue = this._providerFn();
         }
 
