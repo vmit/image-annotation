8898272ea37d8ce68e99ed3a7c6f1b5f4b1cf971
Dmitry Vykochko <dmitry.vykochko@gmail.com>
2017-06-30 21:22:27 +0300

    Rectangle editor

--- src/shape-editors/rectangle-editor.js
+++ src/shape-editors/rectangle-editor.js
@@ -1,4 +1,6 @@
 import BaseShapeEditor from './base-shape-editor';
+import SvgPointDraggable from '../utils/svg/svg-point-draggable';
+import SvgRect from '../utils/svg/svg-rect';
 
 
 export default class RectangleEditor extends BaseShapeEditor {
@@ -11,9 +13,38 @@ export default class RectangleEditor extends BaseShapeEditor {
         super(shape, name);
     }
 
+    _onP1Drag() {
+        this._el.rect.p1 = this._el.p1.point;
+    }
+
+    _onP2Drag() {
+        this._el.rect.p2 = this._el.p2.point;
+    }
+
+    _onDrop() {
+        this.emitUpdate();
+    }
+
     render(canvas) {
         super.render(canvas);
 
+        this._el = {
+            rect: new SvgRect(this.shape.data.p1, this.shape.data.p2, this.canvasSizeProvider),
+            p1: new SvgPointDraggable(this.shape.data.p1, this.canvasSizeProvider, this._onP1Drag.bind(this), this._onDrop.bind(this)),
+            p2: new SvgPointDraggable(this.shape.data.p2, this.canvasSizeProvider, this._onP2Drag.bind(this), this._onDrop.bind(this))
+        };
+
+        this.append(this._el.rect);
+        this.append(this._el.p1);
+        this.append(this._el.p2);
+    }
+
+    rerender() {
+        super.rerender();
+
+        this._el.rect.render();
+        this._el.p1.render();
+        this._el.p2.render();
     }
 
 }
--- src/utils/svg/svg-rect.js
+++ src/utils/svg/svg-rect.js
@@ -5,22 +5,13 @@ export default class SvgRect extends BaseSvgElementWrapper {
     get p1() { return this._p1; }
     set p1(p1) {
         this._p1 = p1;
+        this.render();
     }
 
     get p2() { return this._p2; }
     set p2(p2) {
         this._p2 = p2;
-        if (this._p1 && this._p2) {
-            const x = Math.min(this._p1.x, this._p2.x);
-            const y = Math.min(this._p1.y, this._p2.y);
-            const width = Math.abs(this._p2.x - this._p1.x);
-            const height = Math.abs(this._p2.y - this._p1.y);
-
-            this.set('x', this.toPxX(x));
-            this.set('y', this.toPxY(y));
-            this.set('width', this.toPxX(width));
-            this.set('height', this.toPxY(height));
-        }
+        this.render();
     }
 
     /**
@@ -39,8 +30,17 @@ export default class SvgRect extends BaseSvgElementWrapper {
     }
 
     render() {
-        this.p1 = this._p1;
-        this.p2 = this._p2;
+        if (this._p1 && this._p2) {
+            const x = Math.min(this._p1.x, this._p2.x);
+            const y = Math.min(this._p1.y, this._p2.y);
+            const width = Math.abs(this._p2.x - this._p1.x);
+            const height = Math.abs(this._p2.y - this._p1.y);
+
+            this.set('x', this.toPxX(x));
+            this.set('y', this.toPxY(y));
+            this.set('width', this.toPxX(width));
+            this.set('height', this.toPxY(height));
+        }
     }
 
 }
